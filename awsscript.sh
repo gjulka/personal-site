7eb83ff61b9840dc6c3cc37156d4f17ae59f4b84
gjulka <g.julka10@gmail.com>
2019-06-11 13:37:58 -0500

    ignore fix

--- awsscript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-aws s3 rm s3://www.gauravjulka.me --recursive
-npm run build
-cd ~/Documents/Projects/website4.0/build
-aws s3 sync . s3://www.gauravjulka.me
-cd ~/Documents/Projects/website4.0
